0b5ce7c8b0194d40cbe71c22ac7e60aeee8f7982
MALSI DONALYN <163298892+HallaAnniVu@users.noreply.github.com>
2024-06-21 20:48:31 +0800

    Create navbar.php

---
+++ navbar.php
@@ -0,0 +1,9 @@
+<nav>
+    <ul>
+        <li><a href="course_management.php">Course Management</a></li>
+        <li><a href="enrollment.php">Enrollment</a></li>
+        <li><a href="student_info.php">Student Information</a></li>
+        <li><a href="report.php">Report</a></li>
+        <!-- Add other navbar items as needed -->
+    </ul>
+</nav>
